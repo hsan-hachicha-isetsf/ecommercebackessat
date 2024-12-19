c9eb77509300e15e241db256e23e09cd1e1f4f02
hsan-hachicha-isetsf <hsan.hachicha@gmail.com>
2024-12-19 16:55:51 +0100

    Commit new version

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const cors=require('cors')
 const categoriesRouter=require("./routes/categories.route")
 const articlesRouter=require("./routes/article.route")
 const scategoriesRouter=require("./routes/scategories.route")
+
+const path = require('path'); // Ajout de l'importation de path
 dotenv.config()
 
 //BodyParser Middleware
@@ -16,14 +18,16 @@ mongoose.connect(process.env.DATABASECLOUD)
 .then(() => {console.log("DataBase Successfully Connected");})
 .catch(err => { console.log("Unable to connect to database", err);
 process.exit();});
-app.get("/",(req,res)=>{
 
-    res.send("page accueil")
-
-})
 app.use("/api/categories",categoriesRouter)
 app.use("/api/scategories",scategoriesRouter)
 app.use("/api/articles",articlesRouter)
+
+//dist reactjs
+app.use(express.static(path.join(__dirname, './client/build'))); 
+
+app.get('*', (req, res) => { res.sendFile(path.join(__dirname,'./client/build/index.html')); });
+
 app.listen(process.env.PORT,()=>
     
 console.log(`application exécutée sur le port ${process.env.PORT}`))
